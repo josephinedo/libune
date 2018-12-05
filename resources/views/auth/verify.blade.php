9560088d3a973d021d05f635aa35f0ebd0f0d343
Abrahan Pinedo <joseph.pinedo@gmail.com>
2018-12-05 06:48:31 -0600

    Adding app.js and main.js for auth

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('tema.app')
 
 @section('content')
 <div class="container">
--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,5 +1,5 @@
 <ul class="app-menu">
-    <li><a class="app-menu__item" href="home"><i class="app-menu__icon fa fa-home"></i><span class="app-menu__label">HOME</span></a></li>
+    <li><a class="app-menu__item" href="/home"><i class="app-menu__icon fa fa-home"></i><span class="app-menu__label">HOME</span></a></li>
     <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-user"></i><span class="app-menu__label">Usuarios</span><i class="treeview-indicator fa fa-angle-right"></i></a>
       <ul class="treeview-menu">
         <li><a class="treeview-item" href="{{ route('user.index') }}"><i class="icon fa fa-circle-o"></i> Muestra usuarios</a></li>
